c1be0e7827d7910c4e7402e17fa0269f7d9803d9
xuandai0229 <daicongtu2910@gmail.com>
2025-12-24 18:54:55 +0700

    Initial commit

---
+++ Assets/Scrips/Pathfinder.cs
@@ -0,0 +1,39 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Pathfinder : MonoBehaviour
+{
+    [SerializeField] WaceConfigSO waveConfig;
+    List<Transform> waypoints;
+    int waypointIndex = 0;
+
+    private void Start()
+    {
+        waypoints = waveConfig.GetWaypoints();
+        transform.position = waypoints[waypointIndex].position;
+    }
+
+    void Update()
+    {
+        FollowPath();
+    }
+
+    void FollowPath()
+    {
+        if (waypointIndex < waypoints.Count)
+        {
+            Vector3 targetPosition = waypoints[waypointIndex].position;
+            float delta = waveConfig.GetMoveSpeed() * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, delta);
+            if(transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
